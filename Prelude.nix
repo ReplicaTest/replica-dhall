491d80f59069ec8188fd420563ec08e2b943cedc
Nicolas Biri <nicolas@biri.name>
2024-02-18 21:56:59 +0100

    Remove sha

--- Prelude.nix
+++ Prelude.nix
@@ -4,9 +4,9 @@
     githubBase = "github.com";
     owner = "dhall-lang";
     repo = "dhall-lang";
-    rev = "v22.0.0";
+    rev = "v23.0.0";
     fetchSubmodules = false;
-    sha256 = "157x95yw9hibpifa8figawgclnc1d1bapmbp1zw60l2viy936b83";
+    sha256 = "sha256-AEY5Bifh59XUNkiWiN4D2p5esW6Nko+p0ytX6Jf2swE=";
     directory = "Prelude";
     file = "package.dhall";
     source = false;
